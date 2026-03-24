3f4b78d17f8e34a88c63be8dadf9a358a381a728
MarceloSestrem <145679834+MarceloSestrem@users.noreply.github.com>
2026-03-24 17:41:29 -0300

    Atualizar 'main.ts'

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-//% color="#369ddd"  icon="\uf1b9" block="Teclado"
+//% color=#0fbc11 icon="\uf11c" block="Keypad 4x4"
 namespace keypad4x4 {
 
     let rows: DigitalPin[] = []
@@ -11,7 +11,7 @@ namespace keypad4x4 {
         ["*", "0", "#", "D"]
     ]
 
-    // Inicialização
+    //% block="iniciar keypad linhas %r1 %r2 %r3 %r4 colunas %c1 %c2 %c3 %c4"
     export function init(
         r1: DigitalPin, r2: DigitalPin, r3: DigitalPin, r4: DigitalPin,
         c1: DigitalPin, c2: DigitalPin, c3: DigitalPin, c4: DigitalPin
@@ -20,31 +20,27 @@ namespace keypad4x4 {
         rows = [r1, r2, r3, r4]
         cols = [c1, c2, c3, c4]
 
-        // Configura linhas como saída
         for (let r of rows) {
             pins.digitalWritePin(r, 1)
         }
 
-        // Configura colunas como entrada com pull-up
         for (let c of cols) {
             pins.setPull(c, PinPullMode.PullUp)
         }
     }
 
-    // Lê tecla pressionada
+    //% block="tecla pressionada"
     export function readKey(): string {
 
         for (let i = 0; i < 4; i++) {
 
-            // Ativa uma linha por vez
             pins.digitalWritePin(rows[i], 0)
 
             for (let j = 0; j < 4; j++) {
 
                 if (pins.digitalReadPin(cols[j]) == 0) {
-                    basic.pause(200) // debounce
+                    basic.pause(200)
 
-                    // Espera soltar
                     while (pins.digitalReadPin(cols[j]) == 0) { }
 
                     pins.digitalWritePin(rows[i], 1)
@@ -57,6 +53,4 @@ namespace keypad4x4 {
 
         return ""
     }
-}basic.forever(function () {
-	
-})
+}
